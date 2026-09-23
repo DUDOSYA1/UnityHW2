74b6e47fa635b5dafa5b28b99d34d5a0d434ea25
DUDOSYA <chelovekalfa@gmail.com>
2026-09-23 21:27:03 +0500

    Fixes

--- Assets/CubeRotation.cs
+++ Assets/CubeRotation.cs
@@ -5,12 +5,15 @@ public class CubeRotation : MonoBehaviour
 {
     [SerializeField] private GameObject cube;
     [SerializeField] private int cubeCount;
+
     [SerializeField] private float radius;
     [SerializeField] private bool isAround;
     [SerializeField] private float angleStep;
+
     [SerializeField] private float rotationSpeed;
     [SerializeField] private bool isClockwise;
 
+    private GameObject[] cubesArray;
     void Awake()
     {
         if (cube == null)
@@ -18,6 +21,16 @@ public class CubeRotation : MonoBehaviour
             Debug.LogError("No cube prefab");
             return;
         }
+        cubesArray=new GameObject[cubeCount];
+        for (int i = 0; i < cubeCount; i++) 
+        {
+            cubesArray[i] = Instantiate(cube);
+        }
+        SetCubes();
+    }
+
+    private void SetCubes()
+    {
         var angle = 0f;
         if (isAround)
             angle = (Mathf.PI * 2f) / cubeCount;
@@ -29,21 +42,26 @@ public class CubeRotation : MonoBehaviour
             var x = radius * Mathf.Cos(angle * i);
             var z = radius * Mathf.Sin(angle * i);
 
-            var newCube = Instantiate(cube);
-            newCube.transform.SetParent(transform);
-            newCube.transform.position = new Vector3(x, 0, z);
-            newCube.transform.LookAt(transform);
-
+            cubesArray[i].transform.SetParent(transform);
+            cubesArray[i].transform.localPosition = new Vector3(x, 0, z);
+            cubesArray[i].transform.LookAt(transform);
         }
-
     }
 
-    // Update is called once per frame
+
+    private float prevRadius;
+    private bool prevIsAround;
+    private float prevAngleStep;
     void Update()
     {
         if (isClockwise)
             transform.Rotate(new Vector3(0, rotationSpeed, 0));
         else
             transform.Rotate(new Vector3(0, -rotationSpeed, 0));
+        if (radius != prevRadius || isAround != prevIsAround || angleStep != prevAngleStep )
+            SetCubes();
+        prevRadius = radius;
+        prevIsAround = isAround;
+        prevAngleStep = angleStep;
     }
 }
